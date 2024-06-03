e58b1f4c60fea373f388ed512bfc29cdc492d78e
Thomas Gustafsson <thomas.gustafsson@forsen.com>
2024-06-03 11:12:23 +0200

    Refactor

--- ch3/collatz.py
+++ ch3/collatz.py
@@ -1,17 +1,20 @@
 def collatz(number):
-    integer_number = int(number)
-    if integer_number % 2 == 0:
+    # integer_number = int(number)
+    if number % 2 == 0:
         current_number = number // 2
         print(current_number)
         return current_number
-    elif integer_number % 2 == 1:
-        current_number = 3 * integer_number + 1
+    elif number % 2 == 1:
+        current_number = 3 * number + 1
         print(current_number)
         return current_number
     
 try:
-    num = input("Give me a number: ")
+    num = int(input("Please enter a positive integer: "))
+    if num <= 0:
+        negativeValueError = ValueError('Should be a positive number')
+        raise negativeValueError
     while num != 1:
         num = collatz(num)
 except ValueError:
-    print("Please enter an integer")
+    print("The number should be a positive integer")
