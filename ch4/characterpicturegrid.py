a87c6f2e80c8da6661aca410b8b3df706d25aa2b
Thomas Gustafsson <thomas.gustafsson@forsen.com>
2024-08-19 14:28:20 +0200

    Character Picture Grid added

---
+++ ch4/characterpicturegrid.py
@@ -0,0 +1,15 @@
+# Chapter 4 Practice Project - Character Picture Grid
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+for j in range(len(grid[0])):
+    for i in range(len(grid)):
+        print(grid[i][j],end='')
