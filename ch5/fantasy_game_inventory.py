8028206b96b7ebdc106ed4e45392ea23b25d3e60
Thomas Gustafsson <thomas.gustafsson@forsen.com>
2024-09-02 14:02:49 +0200

    Added add_to_inventory function

--- ch5/fantasy_game_inventory.py
+++ ch5/fantasy_game_inventory.py
@@ -5,9 +5,25 @@ def display_inventory(inventory):
     print("Inventory:")
     item_total = 0
     for k, v in inventory.items():
-        # FILL THIS PART IN
         print(f'{str(v)} {k}')
         item_total += v
     print("Total number of items: " + str(item_total))
 
 display_inventory(stuff)
+
+
+def add_to_inventory(inventory, added_items):
+    # your code goes here
+    for i in added_items:
+        inventory.setdefault(i, 0)
+        inventory[i] = inventory[i] + 1
+    
+    return inventory
+
+inv = {'gold coin': 42, 'rope': 1}
+
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+inv = add_to_inventory(inv, dragon_loot)
+
+display_inventory(inv)
