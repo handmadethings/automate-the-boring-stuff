4f57c60860681ae286d4cb3efe8b7e1a1d452c89
Thomas Gustafsson <thomas.gustafsson@forsen.com>
2024-11-12 13:06:24 +0100

    Minor updates

--- ch13/multiplication_table.py
+++ ch13/multiplication_table.py
@@ -18,17 +18,22 @@ except ValueError:
     print('Please provide an integer argument')
     raise SystemExit(2)
 
+# TODO: Create and populate workbook
+# wb = openpyxl.Workbook()
+# sheet = wb['Sheet']
+
 for i in range(1, table_size):
+    print(f'Set the value of row 1 column {i} to {i}')
+    index_column = f'A{i}'
+    sheet[index_column] = i
+    print(f'Set the value of column 1 row {i} to {i}')
+    column = openpyxl.utils.get_column_letter(i)
+    index_row = f'1{column}'
+    sheet[index_row] = i
     for j in range(1, table_size):
         product = i * j
-        print(f'{i} x {j} is {product}')
-
-# TODO: Create and populate workbook
-wb = openpyxl.Workbook()
-sheet = wb.active
-
-sheet.title = 'Multiplication table'
-
+        print(f'Row {i} column {j} should have the value {product}')
+        # print(f'{i} x {j} is {product}')
 
 # TODO: Save the Excel file
 # wb.save('multiplicationtable.xlsx')
