9757abaadd7e76d8e911e82a94c2a7f2818379da
Thomas Gustafsson <thomas.gustafsson@forsen.com>
2024-11-12 13:20:51 +0100

    Finished multiplication_table.py

--- ch13/multiplication_table.py
+++ ch13/multiplication_table.py
@@ -13,27 +13,24 @@ elif args_count < 2:
 
 try:
     table_size = int(sys.argv[1]) + 1
-    print(f'Table size: {table_size}')
 except ValueError:
     print('Please provide an integer argument')
     raise SystemExit(2)
 
-# TODO: Create and populate workbook
-# wb = openpyxl.Workbook()
-# sheet = wb['Sheet']
+wb = openpyxl.Workbook()
+sheet = wb['Sheet']
 
 for i in range(1, table_size):
-    print(f'Set the value of row 1 column {i} to {i}')
-    index_column = f'A{i}'
+    index_column = f'A{i+1}'
     sheet[index_column] = i
-    print(f'Set the value of column 1 row {i} to {i}')
-    column = openpyxl.utils.get_column_letter(i)
-    index_row = f'1{column}'
+    index_column = openpyxl.utils.get_column_letter(i+1)
+    index_row = f'{index_column}1'
     sheet[index_row] = i
     for j in range(1, table_size):
+        column = openpyxl.utils.get_column_letter(i+1)
+        row = j + 1
         product = i * j
-        print(f'Row {i} column {j} should have the value {product}')
-        # print(f'{i} x {j} is {product}')
+        index = f'{column}{row}'
+        sheet[index] = product
 
-# TODO: Save the Excel file
-# wb.save('multiplicationtable.xlsx')
+wb.save('multiplicationtable.xlsx')
